a2e27e4e728abd26cdd9538481daf05cc1e9f418
liqiang <qiang.li@bangdao-tech.com>
2024-09-25 21:06:13 +0800

    format

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import element from 'element-ui'
-import * as echarts from 'echarts'
-import md5 from 'js-md5';
+import * as echarts from "echarts";
+import element from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import md5 from "js-md5";
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import moment from 'moment';
+import moment from "moment";
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.prototype.$echarts = echarts;
 
 // 使用moment中文语言包
-moment.locale('zh-cn');
+moment.locale("zh-cn");
 
-Vue.use(element)
+Vue.use(element);
 Vue.prototype.$md5 = md5;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
